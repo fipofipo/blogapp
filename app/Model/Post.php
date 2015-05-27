7557b0f1d0875de9637267ac9a87a242f58e61a2
Nanbu Yusue <fipofipo.me@gmail.com>
2015-05-27 22:42:51 +0000

    DocBlockコメントを追加

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -3,12 +3,26 @@ App::uses('AppModel', 'Model');
 /**
  * Post Model
  *
+ * ブログ記事用モデルです
+ *
+ * @copyright php_ci_book
+ * @link https://github.com/phpcibook/blogapp/blob/master/app/Model/Post.php
+ * @since 1.0
+ * @author 南部 <fipofipo.me@gmail.com>
+ *
  */
 class Post extends AppModel {
+
+/**
+ * 一覧表示時のタイトルに使用するカラム名
+ *
+ * @var string
+ *
+ */
 	public $displayField = 'title';
 
 /**
- * Validation rules
+ * バリデーションルール
  *
  * @var array
  */
